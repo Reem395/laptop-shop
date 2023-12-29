986694a6b522f57465bb11b7bfb609f7c878472f
Reem Khalil <76693917+Reem395@users.noreply.github.com>
2023-12-29 02:33:14 +0200

    conflicts resolved

--- lib/business_logic/login_cubit/login_state.dart
+++ lib/business_logic/login_cubit/login_state.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 abstract class LoginState {}
 
 class LoginInitial extends LoginState {}
@@ -18,24 +17,3 @@ class GoogleLoginFailed extends LoginState {}
 
 
 class LogOut extends LoginState {}
-=======
-abstract class LoginState {}
-
-class LoginInitial extends LoginState {}
-
-//Phone Auth
-class PhoneValid extends LoginState {}
-
-class PhoneInvalid extends LoginState {}
-
-class PhoneLoginVerificationSucess extends LoginState {}
-
-class PhoneLoginVerificationFailed extends LoginState {}
- //Google Auth
-class GoogleLoginSuccess extends LoginState {}
-
-class GoogleLoginFailed extends LoginState {}
-
-
-class LogOut extends LoginState {}
->>>>>>> 2f93acf63d83909c339102c7743a7f497c254a9c
